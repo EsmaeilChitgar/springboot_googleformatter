25bfbbf697caca1d1ed9acac9ad6346b1eac09a1
Esmaeil Chitgar <84287711+EsmaeilChitgar@users.noreply.github.com>
2023-06-10 11:39:02 +0330

    initialize project

---
+++ src/main/java/com/example/springboot_googleformatter_plugin_example/Springboot_google_formatter_plugin_exampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot_googleformatter_plugin_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot_google_formatter_plugin_exampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Springboot_google_formatter_plugin_exampleApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/springboot_googleformatter_plugin_example/SpringbootgoogleformatterpluginexampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.springboot_googleformatter_plugin_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootgoogleformatterpluginexampleApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
